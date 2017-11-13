ff141199f5e2eab151004a21018be4ddafb58901
Faymoon <mateo.mercier@laposte.net>
2017-11-13 09:55:42 +0100

    Update Body.h

--- include/Body.h
+++ include/Body.h
@@ -18,16 +18,20 @@
 class Body
 {
 	public:
-		Body(Ndk::EntityHandle entity, const Nz::String materialPath, Nz::LuaInstance& luaRef, const Nz::String luaFunc);
+		Body(Ndk::EntityHandle entity, const Nz::String& texturePath, Nz::LuaInstance& luaRef, const Nz::String& luaFunc);
 		~Body();
 		void Update(const float delta);
 	private:
 		Nz::SpriteRef m_sprite;
 
+		Nz::BoxCollider2DRef m_collider;
+
 		Ndk::EntityHandle m_entity;
 
 		Ndk::GraphicsComponent& m_graphics;
 		Ndk::NodeComponent& m_node;
+		Ndk::PhysicsComponent2D& m_physics;
+		Ndk::CollisionComponent2D& m_collision;
 
 		Nz::LuaInstance& lua;
 		Nz::String m_luaFunc;
