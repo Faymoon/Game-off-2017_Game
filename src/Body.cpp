f3383eb484976d4103b1a9b5419b6b76aa2ebb96
Faymoon <mateo.mercier@laposte.net>
2017-11-13 09:54:42 +0100

    Update Body.cpp

--- src/Body.cpp
+++ src/Body.cpp
@@ -1,19 +1,26 @@
 #include "Body.h"
 
 
-Body::Body(Ndk::EntityHandle entity, const Nz::String texturePath, Nz::LuaInstance& luaRef, const Nz::String luaFunc)
+Body::Body(Ndk::EntityHandle entity, const Nz::String& texturePath, Nz::LuaInstance& luaRef, const Nz::String& luaFunc)
 	:
 	m_entity{entity},
+
 	m_graphics{ m_entity->AddComponent<Ndk::GraphicsComponent>() },
 	m_node{ m_entity->AddComponent<Ndk::NodeComponent>() },
+	m_physics{ m_entity->AddComponent<Ndk::PhysicsComponent2D>() },
+	m_collision{ m_entity->AddComponent<Ndk::CollisionComponent2D>() },
 
 	m_sprite{ Nz::Sprite::New() },
+
+	m_collider{ Nz::BoxCollider2D::New(Nz::Vector2f(100, 500)) },
 
 	lua { luaRef },
 	m_luaFunc{ luaFunc }
 {
+	m_physics.SetMass(1);
 	m_sprite->SetTexture(texturePath);
 	m_graphics.Attach(m_sprite);
+	m_collision.SetGeom(m_collider);
 }
 
 
@@ -28,3 +35,4 @@ void Body::Update(const float delta)
 	lua.Push(m_node.CreateHandle());
 	lua.Call(2, 0);
 }
+
